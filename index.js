694b09af8fa846fa95342d670e8a3f8acbe97227
Ana Radzi <anaradzi@hotmail.com>
2018-12-03 20:45:26 -0800

    added index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-var x = myFunction(4, 3);
+//returning the integer 12, this is a working function
 
-function myFunction(a, b) {
+function multiply(a, b) {
     return a * b;
 }
+
+console.log(multiply(4, 3));
+
+
+
+//function that returns an if else statment (still working on this)
+
+
+function myFunction(person) {    
+    if (person.agerange === "13 to 19") {         console.log("You are a teenager");     } else {         console.log("You're not a teenager");     }
+}
+
+console.log(myFunction("14"));
