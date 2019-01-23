976e42880c4cfba9cbf72f8f30ad077b5f8159fa
Ana Radzi <anaradzi@hotmail.com>
2019-01-22 23:15:52 -0800

    added reminader function and math.random

--- index.js
+++ index.js
@@ -60,3 +60,16 @@ function str() {
     console.log(string2.length);
 }
 str();
+
+//function that returns a random number between 0-10
+
+function Math.floor((Math.random() * 10) + 1);
+console.log(Math.random);
+
+//Reminader of 5/2
+
+function mod(a, b) {
+    return a % b
+}
+
+mod(5, 2)
